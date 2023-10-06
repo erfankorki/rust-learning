f030bfcb3bb7b623db44fd6bcd79d44c4634024e
erfankorki <erfankorki@gmail.com>
2023-10-06 22:39:39 +0330

    [feat] add guessing game project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..100);
+        println!("secret number is {secret_number}");
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to Read the Line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+
+        match secret_number.cmp(&guess) {
+            Ordering::Equal => {
+                println!("guessed right!!!");
+                break;
+            }
+            Ordering::Greater => println!("guessed small"),
+            Ordering::Less => println!("guessed big"),
+        }
+    }
 }
