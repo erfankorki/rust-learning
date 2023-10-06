f8bec79768e9a298499205acf27bb6106e5a43dd
erfankorki <erfankorki@gmail.com>
2023-10-06 20:03:10 +0330

    [feat] add guessing game first version

--- hello_world/target/debug/deps/hello_world-0af4c503821086f2.d
+++
@@ -1,5 +0,0 @@
-/home/erfan/Desktop/Personal/rust-learning/hello_world/target/debug/deps/hello_world-0af4c503821086f2.rmeta: src/main.rs
-
-/home/erfan/Desktop/Personal/rust-learning/hello_world/target/debug/deps/hello_world-0af4c503821086f2.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-ae8808a8fa0178aa.d
+++
@@ -1,5 +0,0 @@
-/home/erfan/Desktop/Personal/rust-learning/hello_world/target/debug/deps/hello_world-ae8808a8fa0178aa: src/main.rs
-
-/home/erfan/Desktop/Personal/rust-learning/hello_world/target/debug/deps/hello_world-ae8808a8fa0178aa.d: src/main.rs
-
-src/main.rs:
